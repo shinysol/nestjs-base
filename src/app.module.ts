1a5c4b96f9e4377efa6f2e2c4fce8d44f1f3a3cd
Park Hyunwoo <hyunwoo.park@izex.co.kr>
2025-02-28 13:55:12 +0900

    prometheus setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { makeCounterProvider, PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [],
+  imports: [PrometheusModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    makeCounterProvider({
+      name: "metric_name",
+      help: "metric_help",
+    }),
+
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectMetric } from '@willsoto/nestjs-prometheus';
+import { Counter } from 'prom-client';
 
 @Injectable()
 export class AppService {
+  constructor(@InjectMetric("metric_name") public counter: Counter<string>) {
+
+  }
   getHello(): string {
     return 'Hello World!';
   }
